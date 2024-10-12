c30781999402dee76700c560296fdafda9653774
Hamidou <122046255+HMAIGA1@users.noreply.github.com>
2024-10-11 21:34:59 -0400

    Main.java
    public class Main{
    }

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
 public class Main{
+  // Main class Method
+  public static void main(String[] args) {
+    // Main System Output
+    System.out.println("Main Class Initialized.");
+    // System Objects
+
+
+    
+  }
 }
