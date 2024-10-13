9f486611a262bea1add681171b9f11df270efebd
Hamidou <122046255+HMAIGA1@users.noreply.github.com>
2024-10-13 15:53:40 +0000

    # Added File MyProgram > main class is MyProgram

---
+++ MyProgram/MyProgram.java
@@ -0,0 +1,8 @@
+package MyProgram;
+public class MyProgram{
+    // Main MyProgram Method
+    public static void main(String[] args) {
+        // Main MyProgram System Output
+        System.out.println("MyProgram is Online:");
+    }
+}
