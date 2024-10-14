7237ca59c922802fb1ebd206c7b9617e88994123
Hamidou <122046255+HMAIGA1@users.noreply.github.com>
2024-10-14 15:32:11 -0400

    Delete MyProgram/MyProgram.java

--- MyProgram/MyProgram.java
+++
@@ -1,8 +0,0 @@
-package MyProgram;
-public class MyProgram{
-    // Main MyProgram Method
-    public static void main(String[] args) {
-        // Main MyProgram System Output
-        System.out.println("MyProgram is Online:");
-    }
-}
