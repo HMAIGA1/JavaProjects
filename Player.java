0b4a28305fa7b0002b388f4f121e1019384160c2
Hamidou <122046255+HMAIGA1@users.noreply.github.com>
2024-10-14 21:52:47 +0000

    10/14/2024

---
+++ Player.java
@@ -0,0 +1,27 @@
+public class Player {
+    /* Person Class Variables and fields */
+    String playerName;
+    char playerGender;
+    int playerAge;
+    String playerHoroscope;
+    int playerGoals;
+    int playerCaptaincy;
+    int playerTrophies;
+    double playerNetWorth;
+    String playerStatus;
+
+;
+    // Constructor and parameters:
+    public Person(String name, char gender, int age, String horoscope, int goals, int captaincy, int trophies, double netWorth, String status) {
+        playerName = name;
+        playerGender = gender;
+        playerAge = age;
+        playerHoroscope = horoscope;
+        playerGoals = goals;
+        playerCaptaincy = captaincy;
+        playerTrophies = throphies;
+        playerNetWorth = netWorth;
+        playerStatus = status;
+    }
+    
+}
