0665f4d4065fdae0405a953b73801f8504d8dd4a
Hamidou <122046255+HMAIGA1@users.noreply.github.com>
2024-11-01 22:17:43 +0000

    Done

---
+++ Account.java
@@ -0,0 +1,14 @@
+public class Account {
+    // Account class Variables
+    double accountBalance;
+    //Constructor 
+    public Account(double balance) {
+    }
+    // Method 
+    public double accountBalance (double balance) {
+        accountBalance = balance;
+        return balance;
+    }
+
+    
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    Account personalAccount = new Account(100000);
+
+
+    // Main System Class
+    public static void main(String [] args) {
+        // Main System Method
+        System.out.println(" Main System Successfully Initialized...");
+    }
+}
